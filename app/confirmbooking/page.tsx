dde0ea0111e15038bf32384e066109150cdebc80
Dada Kingsley <Kingsleydada159@gmail.com>
2025-11-25 23:20:51 -0800

    fixing vercel error

--- app/confirmbooking/page.tsx
+++ app/confirmbooking/page.tsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
-import dynamic from 'next/dynamic';
-const ConfirmBookingSection = dynamic(() => import("@/components/confirmbooking"));
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+const ConfirmBookingSection = dynamic(
+  () => import("@/components/confirmbooking")
+);
 
 const page = () => {
   return (
     <div>
+      <Suspense fallback={<div className="text-white p-10">Loading...</div>}>
         <ConfirmBookingSection />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
