ee40e75319437cdbbe1536252d4db58cf76cbe64
Dada Kingsley <Kingsleydada159@gmail.com>
2025-12-12 16:38:01 -0800

    Changed hero section background

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,58 +15,58 @@ export default function HeroSection() {
       <div className="relative w-full h-[90vh] mt-20">
         {" "}
         <Image
-          src="/hero-img.jpg"
+          src="/2talk-bg-img.jpeg"
           alt="Barber Hero"
           fill={true}
-          className="object-cover opacity-40"
+          className="object-cover opacity-30"
           priority
         />
         {/* Overlay Content */}
         <div className="absolute inset-0 flex flex-col justify-center px-6 md:px-16 space-y-6">
-  <div className="mt-15">
+          <div className="mt-15">
             <div className="gap-2">
-            <motion.h1
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-              className="text-4xl md:text-6xl font-extrabold max-w-2xl leading-tight text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-500 tracking-widest drop-shadow-lg"
-            >
-              H2 <br />
-              Barbershop
-            </motion.h1>
+              <motion.h1
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                className="text-4xl md:text-6xl font-extrabold max-w-2xl leading-tight text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-500 tracking-widest drop-shadow-lg"
+              >
+                H2 <br />
+                Barbershop
+              </motion.h1>
 
-            <motion.h2
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1, delay: 0.3 }}
-              className="text-4xl md:text-5xl font-extrabold max-w-2xl leading-tight text-white drop-shadow-xl"
+              <motion.h2
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.3 }}
+                className="text-4xl md:text-5xl font-extrabold max-w-2xl leading-tight text-white drop-shadow-xl"
+              >
+                Master Your Style
+              </motion.h2>
+            </div>
+
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.6 }}
+              className="text-gray-200 max-w-lg text-lg md:text-xl leading-relaxed mt-5"
             >
-              Master Your Style
-            </motion.h2>
+              Dynamic entertainment services and premium grooming, delivered
+              with precision.
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.9 }}
+              className="flex gap-4 mt-7"
+            >
+              <Link href="/book">
+                <Button className="bg-yellow-600 hover:bg-yellow-500 text-black font-semibold px-6 py-3 rounded-md transition-all duration-300 shadow-md hover:shadow-[#D4A65A]/30">
+                  Book Now
+                </Button>
+              </Link>
+            </motion.div>
           </div>
-
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.6 }}
-            className="text-gray-300 max-w-lg text-lg md:text-xl leading-relaxed mt-5"
-          >
-            Dynamic entertainment services and premium grooming, delivered with
-            precision.
-          </motion.p>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.9 }}
-            className="flex gap-4 mt-7"
-          >
-            <Link href="/book">
-              <Button className="bg-yellow-600 hover:bg-yellow-500 text-black font-semibold px-6 py-3 rounded-md transition-all duration-300 shadow-md hover:shadow-[#D4A65A]/30">
-                Book Now
-              </Button>
-            </Link>
-          </motion.div>
-  </div>
         </div>
       </div>
     </section>
