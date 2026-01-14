dd74ad72189e2c4281852649fcbd2f8928309ce7
Dada Kingsley <Kingsleydada159@gmail.com>
2026-01-14 18:13:25 +0100

    Corrected image src directory

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
       <div className="relative w-full h-[90vh] mt-20">
         {" "}
         <Image
-          src="/2talk-img.jpeg"
+          src="/2talk-bg-img.jpeg"
           alt="Barber Hero"
           fill={true}
           className="object-cover opacity-40"
